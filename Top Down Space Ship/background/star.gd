1232a467f2036a51dd6b84d161d742f2b6523cc0
Patrick <spohrpatrick@gmail.com>
2024-02-19 21:06:28 +0100

    add shader to background color rect

--- Top Down Space Ship/background/star.gd
+++ Top Down Space Ship/background/star.gd
@@ -3,11 +3,21 @@ extends Sprite2D
 @onready var player_ship: CharacterBody2D
 @onready var player_camera: Camera2D
 
+var rng = RandomNumberGenerator.new()
+var randomize_clockwise: int
 
 func _ready() -> void:
 	
 	await get_tree().root.ready
-
+	
+	# can't change instances for canvas_items, so fucked up!
+	#randomize_clockwise = rng.randi_range(0, 10)
+	#print(self, randomize_clockwise)
+	#if randomize_clockwise < 5:
+		#material.set_shader_parameter('clockwise', false)
+		#material.set_shader_parameter('circle_size', Vector2(30.0, 30.0))
+	#else: 
+		#material.set_shader_parameter('clockwise', true)
 
 
 func _process(_delta: float) -> void:
