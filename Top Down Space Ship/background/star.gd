41ab0e76a5309cf26260d31c41d4578ab5eb12dc
Patrick <spohrpatrick@gmail.com>
2024-02-20 08:52:32 +0100

    update star background shader to allow star shader instances

--- Top Down Space Ship/background/star.gd
+++ Top Down Space Ship/background/star.gd
@@ -3,6 +3,7 @@ extends Sprite2D
 @onready var player_ship: CharacterBody2D
 @onready var player_camera: Camera2D
 
+var shader_instance : ShaderMaterial
 var rng = RandomNumberGenerator.new()
 var randomize_clockwise: int
 
@@ -11,15 +12,20 @@ func _ready() -> void:
 	await get_tree().root.ready
 	
 	# can't change instances for canvas_items, so fucked up!
-	#randomize_clockwise = rng.randi_range(0, 10)
-	#print(self, randomize_clockwise)
-	#if randomize_clockwise < 5:
-		#material.set_shader_parameter('clockwise', false)
-		#material.set_shader_parameter('circle_size', Vector2(30.0, 30.0))
-	#else: 
-		#material.set_shader_parameter('clockwise', true)
-
-
+	shader_instance = material.duplicate() # create instance of material
+	randomize_clockwise = rng.randi_range(0, 10)
+	
+	# I DID IT! HELL YEAH! However, the change only applies once when _ready() is called
+	# which means it stops when the next level is selected
+	if randomize_clockwise < 5:
+		#shader_instance.clockwise = false
+		shader_instance.set_shader_parameter('clockwise', false) # change parameter on instance
+		material = shader_instance # apply the new shader instance to material
+	else: 
+		#shader_instance.clockwise = true
+		shader_instance.set_shader_parameter('clockwise', true)
+		material = shader_instance
+	
 func _process(_delta: float) -> void:
 	
 	
